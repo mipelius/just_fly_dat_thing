9ada5d62f6138b2816d4b957fed7437f7112a737
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-16 14:24:34 +0300

    doors / triggers mid-commit

---
+++ Assets/Scripts/DoorButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour {
+
+	public TriggerManager.triggerType triggerType;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		TriggerManager.instance.trigger(triggerType);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		TriggerManager.instance.untrigger(triggerType);
+	}
+}
---
+++ Assets/Scripts/TriggerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour {
+
+	public static TriggerManager instance = null;
+
+	public enum triggerType { yellow, green, purple };
+
+	private List<Door> doors;
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+			doors = new List<Door> ();
+		}
+		else if (instance != this)
+			Destroy(gameObject);
+	}
+
+	public void addDoor(Door door) {
+		doors.Add (door);
+	}
+
+	public void trigger(triggerType triggerType) {
+		foreach (Door door in doors) {
+			if (door.triggerType == triggerType)
+				door.trigger = true;
+		}
+	}
+
+	public void untrigger(triggerType triggerType) {
+		foreach (Door door in doors) {
+			if (door.triggerType == triggerType)
+				door.trigger = false;
+		}
+	}
+}
