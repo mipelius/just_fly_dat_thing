770d7aa7b40e11361e6d9491424eb1cbc3911b04
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-18 09:55:29 +0300

    player physics fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
-	
-	public float rotationSpeed;
-	public float acceleration;
-	public float maxRotation;
-	public float rotationBrakeFactor;
-	public float shockDuration;
-	public float shockTriggerVelocity;
-	public float shockFactor;
+
+	public float rotationVelocity;
+	public float accelerationForce;
+	public float damageThreshold;
 
 	private Rigidbody2D rb;
 
 	private float shockTimeStamp = 0;
 	private float shockPower = 0;
+
+	private bool isColliding = false;
 
 	// Use this for initialization
 	void Start () {
@@ -43,53 +41,29 @@ public class Player : MonoBehaviour {
 	}
 
 	private void Rotate (float rotationInput) {
-		float torque = 0;
-
-		if (rotationInput == 0) {			
-			torque = -Mathf.Sign (rb.angularVelocity) * rotationBrakeFactor;
-
-			if (Mathf.Abs (rb.angularVelocity) < Mathf.Abs (torque)) {
-				rb.angularVelocity = 0;
-				torque = 0;
-			}
-		} else if (			
-			Mathf.Abs (rb.angularVelocity) < maxRotation ||
-			Mathf.Sign (rb.angularVelocity) != Mathf.Sign (-rotationInput)
-		) {
-			torque = -rotationInput * rotationSpeed;		
+		if (rotationInput == 0) {
+			rb.angularVelocity = 0;
+		} else if (Mathf.Abs(rb.angularVelocity) < rotationVelocity) {
+			rb.angularVelocity = -rotationInput * rotationVelocity;
 		}
-
-		float torqueShockFactor = Mathf.Abs (shockTimeStamp - Time.time) / shockDuration;
-		torqueShockFactor = Mathf.Pow (torqueShockFactor, shockPower);
-
-		if (torqueShockFactor > 1) {
-			torqueShockFactor = 1;		
-		}
-
-		float actualTorque = torque * torqueShockFactor;
-
-		rb.AddTorque (actualTorque);		
 	}
 
 	private void Accelerate (float accelerationInput) {
-		
-		float accAmount = accelerationInput * acceleration;
+		float accAmount = accelerationInput * accelerationForce;
 		float accAngle = (transform.rotation.eulerAngles.z / 360) * Mathf.PI * 2;
 
-		Vector3 force = new Vector3 (
-			Mathf.Cos (accAngle) * accAmount,
-			Mathf.Sin (accAngle) * accAmount,
-			0
-		);
-
+		Vector2 force = new Vector2 (
+			               Mathf.Cos (accAngle) * accAmount,
+			               Mathf.Sin (accAngle) * accAmount			               
+		               );
+	
 		rb.AddForce (force);
 	}
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.relativeVelocity.magnitude > shockTriggerVelocity) {
-			shockTimeStamp = Time.time;
-			shockPower = collision.relativeVelocity.magnitude * shockFactor;
+		if (collision.relativeVelocity.magnitude > damageThreshold) {
+			// damage
 		}
 	}
 
