585fc92e754ce2029af5333cd6bd15f219d053af
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-18 17:05:13 +0300

    minor fixes

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -7,12 +7,10 @@ public class Loader : MonoBehaviour {
 	public bool levelDevelopmentMode;
 
 	public GameObject debugCanvas;
-	public GameObject eventSystem;
 
 	void Awake () {
 		if (levelDevelopmentMode) {			
 			debugCanvas = Instantiate (debugCanvas); DontDestroyOnLoad (debugCanvas);
-			eventSystem = Instantiate (eventSystem); DontDestroyOnLoad (eventSystem);
 		} else {
 			UnityEngine.SceneManagement.SceneManager.LoadScene ("ScreenMain");
 		}
