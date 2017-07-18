cd52c4979da5c6909a4ec04ca7d0484d4eedd48e
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-18 11:36:28 +0300

    base

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+
+	public GameObject playerToInstantiate;
+
+	void Awake () {
+		GameObject player = Instantiate (playerToInstantiate);
+
+		player.transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z + 90);
+
+		float scale = transform.lossyScale.y / 20;
+		float angle = (transform.eulerAngles.z + 90 )/ 360 * 2 * Mathf.PI;
+
+		Vector3 transmission = new Vector3(
+			Mathf.Cos(angle) * scale,
+			Mathf.Sin(angle) * scale,
+			0
+		);
+
+		player.transform.position = transform.position + transmission;
+
+		GameObject camera = GameObject.Find ("Main Camera");
+		CameraBehaviour cameraBehaviour = camera.GetComponent<CameraBehaviour>();
+		cameraBehaviour.playerToFollow = player;
+	}
+}
--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -6,18 +6,15 @@ public class CameraBehaviour : MonoBehaviour {
 
 	public GameObject playerToFollow;
 
-
-	// Use this for initialization
-	void Start () {
-
-	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.position = new Vector3 (
-			playerToFollow.transform.position.x,
-			playerToFollow.transform.position.y,
-			-10
-		);
+		if (playerToFollow != null) {
+			transform.position = new Vector3 (
+				playerToFollow.transform.position.x,
+				playerToFollow.transform.position.y,
+				-10
+			);
+		}
 	}
 }
