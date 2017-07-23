782fa81e8b78189bbb291722c6862110f14dc1f2
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-23 19:44:45 +0300

    falling block fix

--- Assets/Scripts/Game/FallingBlock.cs
+++ Assets/Scripts/Game/FallingBlock.cs
@@ -3,31 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FallingBlock : MonoBehaviour {
+	
+	public GameObject fallingBlockExplosion;
+
+	public float pushForce;
 
 	private Vector3 originalPosition;
 	private Quaternion originalRotation;
 
 	private Rigidbody2D rb;
 
-	// Use this for initialization
+
 	void Start () {
 		originalPosition = transform.position;
 		originalRotation = transform.rotation;
 		rb = GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		Explode ();
 	}
 
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "wall") {
-			transform.position = originalPosition;
-			transform.rotation = originalRotation;
-			rb.velocity = new Vector2(0, 0);
-			rb.angularVelocity = 0;
+
+	void Explode() {
+		Instantiate (fallingBlockExplosion, this.transform.position, Quaternion.identity);
+
+		transform.position = originalPosition;
+		transform.rotation = originalRotation;
+		rb.velocity = new Vector2(0, 0);
+		rb.angularVelocity = 0;
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.gameObject.tag == "Player") {
+			collision.rigidbody.AddForce (rb.velocity.normalized * pushForce);
 		}
+		Explode ();
 	}
 }
