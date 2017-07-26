70fac32b268bae8654b15dfac95da63a36cdf31a
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-26 14:17:29 +0300

    UI -> highscores

---
+++ Assets/Scripts/UI_Menu/ScreenMain/HighScoreLevelButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.EventSystems;
+
+public class HighScoreLevelButton : MonoBehaviour {
+
+	private Button button;
+
+	public int levelNumber;
+
+	public GameObject levelNameText;
+	public GameObject levelImage;
+
+	void Start () {
+		button = GetComponent<Button> ();
+
+		Level level = LevelManager.instance.GetLevel (levelNumber);
+
+		if (level != null) {
+			Text text = levelNameText.GetComponent<Text> ();
+			text.text = level.name;
+
+			RawImage image = levelImage.GetComponent<RawImage> ();
+			string imagePath = Path.Combine ("LevelImages", level.levelImage);
+			image.texture = Resources.Load<Texture2D> (imagePath);
+		}	
+	}
+
+	public void MouseClick() {
+		HighscorePanel.instance.SetCurrentLevel (levelNumber);
+	}
+}
---
+++ Assets/Scripts/UI_Menu/ScreenMain/HighscorePanel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscorePanel : MonoBehaviour {
+
+	public static HighscorePanel instance;
+
+	public GameObject HighscoreView;
+
+	public GameObject[] playerNameTexts;
+	public GameObject[] scoreTexts;
+
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		}
+		else if (instance != this)
+			Destroy(gameObject);	
+	}
+
+	public void SetCurrentLevel(int levelNumber) {
+		HighscoreView.SetActive (true);
+
+		Level level = LevelManager.instance.GetLevel (levelNumber);
+
+		List<Score> scores = level.scores;
+
+		scores.Sort ();
+		scores.Reverse ();
+
+		for (int i = 0; i < 10; i++) {			
+			Text playerText = playerNameTexts[i].GetComponent<Text> ();
+			Text scoreText = scoreTexts[i].GetComponent<Text> ();
+
+			if (i < scores.Count) {
+				User user = UserManager.instance.GetUser (scores [i].user_id);
+
+				playerText.text = user.name;
+				scoreText.text = scores [i].score.ToString();
+			} else {
+				playerText.text = "";
+				scoreText.text = "";
+			}
+		}
+	}
+
+	public void HideScoreview () {
+		HighscoreView.SetActive (false);
+	}
+}
