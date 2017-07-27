a31d7f85c059311aadc00dab6b183538d47208b1
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-27 16:43:48 +0300

    camera zoom in/out

--- Assets/Scripts/Game/CameraBehaviour.cs
+++ Assets/Scripts/Game/CameraBehaviour.cs
@@ -6,7 +6,19 @@ public class CameraBehaviour : MonoBehaviour {
 
 	public GameObject playerToFollow;
 
-	
+	public float zoomInSize;
+	public float zoomOutSize;
+
+	public float zoomingRate;
+
+	private bool zoomingIn = true;
+
+	private Camera camera;
+
+	void Awake() {
+		camera = GetComponent<Camera> ();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (playerToFollow != null) {
@@ -15,6 +27,30 @@ public class CameraBehaviour : MonoBehaviour {
 				playerToFollow.transform.position.y,
 				-10
 			);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			zoomingIn = !zoomingIn;
+		}
+
+		HandleZooming ();
+	}
+
+	private void HandleZooming() {
+		if (zoomingIn) {
+			if (camera.orthographicSize	> zoomInSize) {
+				camera.orthographicSize -= zoomingRate * Time.deltaTime;
+			}
+			if (camera.orthographicSize < zoomInSize) {
+				camera.orthographicSize = zoomInSize;
+			}
+		} else {
+			if (camera.orthographicSize	< zoomOutSize) {
+				camera.orthographicSize += zoomingRate * Time.deltaTime;
+			}
+			if (camera.orthographicSize > zoomOutSize) {
+				camera.orthographicSize = zoomOutSize;
+			}			
 		}
 	}
 }
