8d509d26a4919897ff5e3bda32b9d9e310e7cf5f
Miika Pelkonen <pelkonen.miika@gmail.com>
2017-07-27 16:24:45 +0300

    sprite update

--- Assets/Scripts/Game/Exit.cs
+++ Assets/Scripts/Game/Exit.cs
@@ -6,8 +6,9 @@ public class Exit : MonoBehaviour {
 
 	public bool canUse = false;
 
-	public Sprite exitOpenSprite;
-	public Sprite exitClosedSprite;
+	public GameObject redEyeParticle;
+	public GameObject blueEyeParticle;
+	public GameObject exitShieldParticle;
 
 	private SpriteRenderer spriteRenderer;
 
@@ -17,13 +18,21 @@ public class Exit : MonoBehaviour {
 	}
 
 	void Update() {
-		if (canUse) {
-			spriteRenderer.sprite = exitOpenSprite;
-		} else {
-			spriteRenderer.sprite = exitClosedSprite;
-		}
+		Collider2D collider = GetComponent<Collider2D> ();
 
+		if (canUse) {
+			exitShieldParticle.SetActive (false);
+			redEyeParticle.SetActive (false);
+			blueEyeParticle.SetActive (true);
+			collider.isTrigger = true;
+		} else {
+			exitShieldParticle.SetActive (true);
+			redEyeParticle.SetActive (true);
+			blueEyeParticle.SetActive (false);
+			collider.isTrigger = false;
+		}
 	}
+
 	private void OnTriggerEnter2D (Collider2D other) {
 		if (other.tag == "Player" && canUse) {
 			UILevelManager.instance.LevelFinished ();
---
+++ Assets/Scripts/Game/ExitShield.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitShield : MonoBehaviour {	
+	public float damageAmount;
+
+	void OnTriggerStay2D(Collider2D collider) {		
+		if (collider.tag == "Player") {
+			collider.gameObject.SendMessage ("ApplyDamage", damageAmount * Time.deltaTime);
+		}
+	}
+}
